8392bca671703f4cd4539e042d0ea767ba829f91
Sebastian Hofmann <hofmann.s@uni-jena.de>
2016-10-07 13:21:10 +0200

    MCR-1396 IIIF-Presentation-API
    Moved all existing iiif stuff into new MyCoRe module

---
+++ mycore-iiif/src/main/java/org/mycore/iiif/image/impl/MCRIIIFUnsupportedFormatException.java
@@ -0,0 +1,9 @@
+package org.mycore.iiif.image.impl;
+
+
+public class MCRIIIFUnsupportedFormatException extends Exception {
+
+    public MCRIIIFUnsupportedFormatException(String unsupportedFormat) {
+        super("The format " + unsupportedFormat + " is not supported!");
+    }
+}
